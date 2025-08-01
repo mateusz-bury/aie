edaaef34f267dc9bb9bef736a7d92c443227a19d
Jakub Cepielik <jakubcepielik168@gmail.com>
2025-08-01 20:41:08 +0200

    poprawa brancha

---
+++ backend/AIO_API/Controllers/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using AIO_API.Data;
+using AIO_API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIO_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private ICharacterService _characterService;
+        private readonly AieDbContext _dbContext;
+
+        public CharacterController(ICharacterService characterService, AieDbContext dbContext)
+        {
+            _characterService = characterService;
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<ICharacter>> GetAllCharactersAsync()
+        {
+            //return await _characterService.GetAllCharactersAsync();
+            var playableCharacters = _dbContext.
+                                    PlayableCharacter.
+                                    ToList();
+            return playableCharacters;
+        }
+    }
+}
---
+++ backend/AIO_API/Data/AieDbContext.cs
@@ -0,0 +1,38 @@
+﻿using AIO_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AIO_API.Data
+{
+    public class AieDbContext : DbContext
+    {
+        public DbSet<PlayableCharacter> PlayableCharacter { get; set; }
+
+        private string _connectionString = "Server=localhost\\SQLEXPRESS;Database=AieDb;Trusted_Connection=True;";
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PlayableCharacter>()
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<PlayableCharacter>()
+                .Property(p => p.Race)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<PlayableCharacter>()
+                .Property(p => p.Career)
+                .IsRequired()
+                .HasMaxLength(50);
+            modelBuilder.Entity<PlayableCharacter>()
+                .Property(p => p.Age)
+                .IsRequired();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
